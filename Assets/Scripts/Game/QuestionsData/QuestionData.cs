5717ad7a2832368b7bbccb37857b6fb5058c8768
Jonathan Soto Aguilar <jonathan.soto.a@usach.cl>
2024-10-09 15:55:45 -0300

    Asset Bundle Test

--- Assets/Scripts/Game/QuestionsData/QuestionData.cs
+++
@@ -1,8 +0,0 @@
-[System.Serializable]
-public class QuestionData
-{
-    public string question;
-    public string[] answers;
-    public int indexCorrectAnswer;
-    public int scoreForCorrectAnswer;
-}
--- Assets/Scripts/Square/Square.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public abstract class Square : MonoBehaviour
-{
-    // Método abstracto para activar la casilla y devolver una corrutina
-    public abstract IEnumerator ActiveSquare(PlayerData player, CanvasPlayer canvasPlayer);
-}
